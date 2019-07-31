242f43affa0ab8da9812acf350e269769f08f767
EugeneTereschenko <tereschenko.eugene@gmail.com>
2019-07-31 15:06:27 +0300

    fix

--- js/square.js
+++ js/square.js
@@ -18,8 +18,6 @@ function myFunction() {
 function myTrianFunction() {
   let str_width = document.getElementById("width_trian").value;
   let str_height = document.getElementById("height_trian").value;
-  console.log(str_width);
-  console.log(str_height);
   document.getElementById("triangle-up").style.width = str_width + "px";
   document.getElementById("triangle-up").style.height = str_height + "px";
 }
@@ -27,8 +25,13 @@ function myTrianFunction() {
 function parallFunction() {
   let str_width = document.getElementById("width_parall").value;
   let str_height = document.getElementById("height_parall").value;
-  console.log(str_width);
-  console.log(str_height);
   document.getElementById("parallelogram").style.width = str_width + "px";
   document.getElementById("parallelogram").style.height = str_height + "px";
+}
+
+function radiusFunction() {
+  let str_width = document.getElementById("radius_para").value;
+  let str_height = str_width;
+  document.getElementById("circle").style.width = str_width + "px";
+  document.getElementById("circle").style.height = str_height + "px";
 }
